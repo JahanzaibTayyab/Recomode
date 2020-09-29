948559282dcc517f68f33676a2cb118d214b99c6
JahanzaibTayyab <jtayyab204@gmail.com>
2020-09-29 10:42:23 +0500

    Add firebase config file

---
+++ app/config/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDWEIAU7qkeLq5LbfA7yVHLwl1ZlCGGXRg',
+  authDomain: 'recomode-5a12b.firebaseapp.com',
+  databaseURL: 'https://recomode-5a12b.firebaseio.com',
+  projectId: 'recomode-5a12b',
+  storageBucket: 'recomode-5a12b.appspot.com',
+  messagingSenderId: '938951481573',
+  appId: '1:938951481573:web:ebc45d050510fd80a6dc0c',
+  measurementId: 'G-2MM7ZRPCZC',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
+
+export const f = firebase;
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
