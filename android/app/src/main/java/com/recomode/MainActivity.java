252502a2c7898dd1c22bd54eff8cd247cc0abba3
JahanzaibTayyab <jtayyab204@gmail.com>
2020-11-28 11:01:51 +0500

    update splash screen

--- android/app/src/main/java/com/recomode/MainActivity.java
+++ android/app/src/main/java/com/recomode/MainActivity.java
@@ -1,22 +1,42 @@
 package com.soft.recomode;
 
+
+import android.content.Intent;
+import android.content.Context;
+
 import com.facebook.react.ReactActivity;
-import org.devio.rn.splashscreen.SplashScreen;
-import android.os.Bundle;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.facebook.react.modules.i18nmanager.I18nUtil;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "Recomode";
-  }
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-      SplashScreen.show(this);
-      super.onCreate(savedInstanceState);
-  }
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+      return "Recomode";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+        I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
+        Context context = getApplicationContext();
+        sharedI18nUtilInstance.allowRTL(context, true);
+    }
 }
